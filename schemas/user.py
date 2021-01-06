593c5cc55fb10257a7d68b431723d211d16f800a
Abhishek T C <abhishekgowda4258@gmail.com>
2021-01-06 13:01:28 +0530

    Section 4 Advance Email

---
+++ schemas/user.py
@@ -0,0 +1,16 @@
+from marshmallow import pre_dump
+
+from ma import ma
+from models.user import UserModel
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = UserModel
+        load_only = ("password",)
+        dum_only = ("id", "confirmation")
+        load_instance = True
+
+        @pre_dump
+        def _pre_dump(self, user: UserModel):
+            user.confirmation = [user.most_recent_confirmation]
+            return user
