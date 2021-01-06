80c2e7eb182c8e657910c63481b584f4c08005e5
Abhishek T C <abhishekgowda4258@gmail.com>
2021-01-06 12:52:54 +0530

    Section 1

---
+++ code/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+
+from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from blacklist import BLACKLIST
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.secret_key = 'abhishek' #app.config['JWT_SECRET_KEY']
+api = Api(app) 
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWTManager(app)
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ code/resources/store.py
@@ -0,0 +1,42 @@
+from flask_restful import Resource
+from models.store import StoreModel
+
+NAME_ALREADY_EXISTS = "An store with name '{}' already exists."
+ERROR_OCCURED_CREATING = "An error occurred creating the store."
+STORE_DELETED = "Store deleted."
+STORE_NOT_FOUND = "Store not found."
+
+class Store(Resource):
+    @classmethod
+    def get(cls, name: str):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {'message': STORE_NOT_FOUND}, 404
+
+    @classmethod
+    def post(cls, name:str):
+        if StoreModel.find_by_name(name):
+            return {'message': NAME_ALREADY_EXISTS.format(name)}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {"message": ERROR_OCCURED_CREATING}, 500
+
+        return store.json(), 201
+
+    @classmethod
+    def delete(cls, name:str):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message': STORE_DELETED}
+
+
+class StoreList(Resource):
+    @classmethod
+    def get(cls):
+        return {'stores': [ x.json() for x in StoreModel.find_all()]}
